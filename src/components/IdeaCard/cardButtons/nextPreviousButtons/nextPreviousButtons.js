7e45418ca80836360f3b5b68f19b2a3e540e9fd6
Eyal Gromzy <Eyal_Gromzy@contractor.amat.com>
2018-07-29 15:51:12 +0300

    update project structure

--- src/components/IdeaCard/cardButtons/nextPreviousButtons/nextPreviousButtons.js
+++ src/components/IdeaCard/cardButtons/nextPreviousButtons/nextPreviousButtons.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react'
 import NextButton from './nextButton/nextButton'
 import PreviousButton from './previousButton/previousButton'
 
-export default class nextPreviousButtons extends Component {
+export default class IdeaNextPreviousButtons extends Component {
   render() {
     return (
       <div>
