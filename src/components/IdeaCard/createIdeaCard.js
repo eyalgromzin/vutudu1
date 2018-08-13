833a77afec8af05266cb12f9b923797904b88e8d
Eyal Gromzy <Eyal_Gromzy@contractor.amat.com>
2018-08-13 16:52:31 +0300

    create add idea page 1

--- src/components/IdeaCard/createIdeaCard.js
+++ src/components/IdeaCard/createIdeaCard.js
@@ -1,17 +1,31 @@
 import React, { Component } from 'react'
 import IdeaAttachmentsButton from './cardButtons/addAttachmentButton/addAttachmentButton'
 import IdeaPlace from './cardButtons/placeButton/placeButton'
+import './createIdeaCard.css'
 
 export default class CreateIdeaCard extends Component {
+constructor(props){
+  super(props);
+
+  this.state ={
+    title: "",
+    countent: ""
+  }
+
+  this.handleChange = this.handleChange.bind(this);
+}
+
+handleChange(event) {
+    this.setState({value: event.target.value});
+}
+
   render() {
     return (
-      <div id="ideaCard">
-          <div id="ideaCard"> 
-            <div id="ideaTitle"> title </div>
-            <div id="ideaMainContent">
-              <div id="ideaContent"> content </div>
-            </div>
-          </div>
+      <div id="ideaCard"> 
+        <div id="ideaCardContent">
+          <input type="text" id="newIdeaTitle" value={this.state.title} placeholder="<title>" onchange={this.handleChange}/>
+          <textarea type="text" id="newIdeaContent" value={this.state.content} placeholder="<content>" onchange={this.handleChange}/>
+        </div>
       </div>
     )
   }
