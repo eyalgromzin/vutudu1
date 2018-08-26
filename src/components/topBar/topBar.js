a034efcf7cfa766fd52ec317c9213ad6e141c50b
eyalgromzin <ilyagromzy@gmail.com>
2018-08-26 23:11:39 +0300

    add facebook button

--- src/components/topBar/topBar.js
+++ src/components/topBar/topBar.js
@@ -4,6 +4,7 @@ import 'commonCss.css'
 import { CHANGE_PAGE_TO_CREATE_IDEA } from  'redux/commonReducer'
 import { CHANGE_PAGE_TO_SHOW_IDEAS } from   'redux/commonReducer'
 import { connect } from 'react-redux';
+import Facebook from 'components/facebook'
 
 class TopBar extends Component {
   constructor(props){
@@ -13,6 +14,7 @@ class TopBar extends Component {
     this.handleShowIdeasClick = this.handleShowIdeasClick.bind(this);
     this.changePageToCreateIdea = this.changePageToCreateIdea.bind(this);
     this.changePageToShowIdeas = this.changePageToShowIdeas.bind(this);
+    this.handleLogin = this.handleLogin.bind(this);
   }
 
   handleNewIdeaClick(){
@@ -31,14 +33,22 @@ class TopBar extends Component {
     this.props.dispatch({ type: "CHANGE_PAGE_TO_SHOW_IDEAS" });
   }
 
+  //app id = "1886935948065131"
+  handleLogin = () => {
+
+  }
+
   render() {
     return (
       <div id="TopBar mainContent">
         <div class="mainContent"> 
           <img src={require("images/logo3.png")} class="topBarLogo verticalMiddleAlign" />
           <div id="mainLogo" class=""> VUTUDU </div> 
+          {/* <img id="login" src={require("images/user.png")} class="topBarIcon verticalMiddleAlign alignRight" onClick={this.handleLogin} /> */}
+          <Facebook />
           <img id="newIdeaButton" src={require("images/newIdea.png")} class="topBarIcon verticalMiddleAlign alignRight" onClick={this.handleNewIdeaClick} />
           <img id="searchIdeasButton" src={require("images/search.png")} class="topBarIcon verticalMiddleAlign alignRight" onClick={this.handleShowIdeasClick} />
+          
         </div>
       </div>  
     )
